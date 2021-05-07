7d8a955da4ac4d560b7c5f739658b271b66ed71a
Rosanne Wallach <66342005+RosanneWallach@users.noreply.github.com>
2021-05-06 23:35:10 -0400

    Add files via upload

---
+++ Project3/Capstone-Project/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import React from "react";
+import Calculator from "./Calculator.js";
+
+
+class App extends React.Component {
+ 
+  render() {
+    return (
+      <Calculator></Calculator>
+    )
+  }
+}
+
+export default App;
+
+
+ 
